0cde00586c764bad61c09dd48875be51fce2e218
Admin <git@webgk.ru>
2017-01-30 11:37:20 +0300

    create

---
+++ sect_footer_left.php
@@ -0,0 +1,7 @@
+<div class="logo">
+	<?if(MAIN_PAGE === TRUE):?>
+		<span><img src="<?=SITE_TEMPLATE_PATH?>/images/logopod.png" alt=""></span>
+	<?else:?>
+		<a href="<?=SITE_DIR?>"><img src="<?=SITE_TEMPLATE_PATH?>/images/logopod.png" alt=""></a>
+	<?endif;?>
+</div>
