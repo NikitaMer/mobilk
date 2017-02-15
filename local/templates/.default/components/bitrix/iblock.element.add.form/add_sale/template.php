b533fdae824418ec2f03211f9d49e0da7fab5fd9
Mihan <lm@webgk.ru>
2017-02-15 11:33:41 +0300

    SUP-5700: Product choise changed

--- local/templates/.default/components/bitrix/iblock.element.add.form/add_sale/template.php
+++ local/templates/.default/components/bitrix/iblock.element.add.form/add_sale/template.php
@@ -123,32 +123,70 @@ if (!empty($arResult["ERRORS"])):?>
 						</div>
 					</div>
 					<div class="webFormItemField">
-						<?$APPLICATION->IncludeComponent("bitrix:search.title", "products_search", Array(
-							"COMPONENT_TEMPLATE" => ".default",
-								"NUM_CATEGORIES" => "1",	// Количество категорий поиска
-								"TOP_COUNT" => "15",	// Количество результатов в каждой категории
-								"ORDER" => "date",	// Сортировка результатов
-								"USE_LANGUAGE_GUESS" => "Y",	// Включить автоопределение раскладки клавиатуры
-								"CHECK_DATES" => "N",	// Искать только в активных по дате документах
-								"SHOW_OTHERS" => "N",	// Показывать категорию "прочее"
-								"PAGE" => "#SITE_DIR#search/index.php",	// Страница выдачи результатов поиска (доступен макрос #SITE_DIR#)
-								"SHOW_INPUT" => "Y",	// Показывать форму ввода поискового запроса
-								"INPUT_ID" => "title-search-input",	// ID строки ввода поискового запроса
-								"CONTAINER_ID" => "title-search",	// ID контейнера, по ширине которого будут выводиться результаты
-								"CATEGORY_0_TITLE" => "",	// Название категории
-								"CATEGORY_0" => array(	// Ограничение области поиска
-									0 => "iblock_catalog",
+						<?$APPLICATION->IncludeComponent("bitrix:news.list", "product_choise", Array(
+								"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
+								"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
+								"AJAX_MODE" => "N",	// Включить режим AJAX
+								"AJAX_OPTION_ADDITIONAL" => "",	// Дополнительный идентификатор
+								"AJAX_OPTION_HISTORY" => "N",	// Включить эмуляцию навигации браузера
+								"AJAX_OPTION_JUMP" => "N",	// Включить прокрутку к началу компонента
+								"AJAX_OPTION_STYLE" => "Y",	// Включить подгрузку стилей
+								"CACHE_FILTER" => "N",	// Кешировать при установленном фильтре
+								"CACHE_GROUPS" => "Y",	// Учитывать права доступа
+								"CACHE_TIME" => "36000000",	// Время кеширования (сек.)
+								"CACHE_TYPE" => "A",	// Тип кеширования
+								"CHECK_DATES" => "Y",	// Показывать только активные на данный момент элементы
+								"DETAIL_URL" => "",	// URL страницы детального просмотра (по умолчанию - из настроек инфоблока)
+								"DISPLAY_BOTTOM_PAGER" => "N",	// Выводить под списком
+								"DISPLAY_DATE" => "N",	// Выводить дату элемента
+								"DISPLAY_NAME" => "Y",	// Выводить название элемента
+								"DISPLAY_PICTURE" => "N",	// Выводить изображение для анонса
+								"DISPLAY_PREVIEW_TEXT" => "N",	// Выводить текст анонса
+								"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
+								"FIELD_CODE" => array(	// Поля
+									0 => "ID",
+									1 => "NAME",
+									2 => "",
 								),
-								"CATEGORY_0_iblock_catalog" => array(	// Искать в информационных блоках типа "iblock_catalog"
-									0 => "14",
-									1 => "20"
-								)
+								"FILTER_NAME" => "",	// Фильтр
+								"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
+								"IBLOCK_ID" => "20",	// Код информационного блока
+								"IBLOCK_TYPE" => "catalog",	// Тип информационного блока (используется только для проверки)
+								"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
+								"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
+								"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
+								"NEWS_COUNT" => "",	// Количество новостей на странице
+								"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
+								"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
+								"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
+								"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
+								"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
+								"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
+								"PAGER_TITLE" => "Новости",	// Название категорий
+								"PARENT_SECTION" => "",	// ID раздела
+								"PARENT_SECTION_CODE" => "",	// Код раздела
+								"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
+								"PROPERTY_CODE" => array(	// Свойства
+									0 => "",
+									1 => "",
+								),
+								"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
+								"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
+								"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
+								"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
+								"SET_STATUS_404" => "N",	// Устанавливать статус 404
+								"SET_TITLE" => "N",	// Устанавливать заголовок страницы
+								"SHOW_404" => "N",	// Показ специальной страницы
+								"SORT_BY1" => "ACTIVE_FROM",	// Поле для первой сортировки новостей
+								"SORT_BY2" => "SORT",	// Поле для второй сортировки новостей
+								"SORT_ORDER1" => "DESC",	// Направление для первой сортировки новостей
+								"SORT_ORDER2" => "ASC",	// Направление для второй сортировки новостей
+								"COMPONENT_TEMPLATE" => ".default"
 							),
 							false
 						);?>
 					</div>
 				</div>
-				<input type="hidden" name="PROPERTY[<?= SALER_REPORT_PRODUCT_ID_PROPERTY_ID ?>][0]" size="25" value="" />
 				<? } ?>
 			<? } ?>
 		</div>
---
+++ local/templates/.default/components/bitrix/news.list/product_choise/template.php
@@ -0,0 +1,19 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+/** @var array $arParams */
+/** @var array $arResult */
+/** @global CMain $APPLICATION */
+/** @global CUser $USER */
+/** @global CDatabase $DB */
+/** @var CBitrixComponentTemplate $this */
+/** @var string $templateName */
+/** @var string $templateFile */
+/** @var string $templateFolder */
+/** @var string $componentPath */
+/** @var CBitrixComponent $component */
+$this->setFrameMode(true);
+?>
+<select name="PROPERTY[<?= SALER_REPORT_PRODUCT_ID_PROPERTY_ID ?>][0]" id="">
+<? foreach ($arResult["ITEMS"] as $arItem) { ?>
+	<option value="<?= $arItem['ID'] ?>"><?= $arItem['NAME'] ?></option>
+<? } ?>
+</select>
