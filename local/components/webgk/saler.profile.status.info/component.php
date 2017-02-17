dd0ce62d08557b1fa54511e48154b8752d55984f
Mihan <lm@webgk.ru>
2017-02-17 14:47:37 +0300

    SUP-5700: Second added realized

--- local/components/webgk/saler.profile.status.info/component.php
+++ local/components/webgk/saler.profile.status.info/component.php
@@ -1,12 +1,19 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 global $USER;
+
+$total_points = getTotalUserPoints($USER->GetID());
 // узнаем статус пользователя
 if (isUserHaveGoldenStatus($USER->GetID())) {
 	$arResult['STATUS'] = GetMessage("GOLD");
 } else {
 	$arResult['STATUS'] = GetMessage("SILVER");
+	// получаем кол-во баллов до следующего уровня
+	$borders = getLevelsBorders();
+	$arResult['POINTS_TO_NEXT_LVL'] = $borders[SILVER_LVL_ID]['to'] - $total_points;
 }
+// общее кол-во баллов за все время
+$arResult['TOTAL_POINTS'] = $total_points;
 
 // получаем кол-во баллов
 $arResult['BALANCE'] = getUserPoints($USER->GetID());
--- local/components/webgk/saler.profile.status.info/lang/ru/component.php
+++ local/components/webgk/saler.profile.status.info/lang/ru/component.php
@@ -1,4 +1,6 @@
 <?
 $MESS['SILVER'] = "Серебряный партнер";
 $MESS['GOLD'] = "Золотой партнер";
+$MESS['POINTS_TO_NEXT_LVL'] = "До уровня золотого партнера осталось points баллов";
+$MESS['TOTAL_POINTS'] = "Всего заработано points баллов";
 ?>
